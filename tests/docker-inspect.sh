c582a643572a8cad465ae2d6de588c8e909cea0d
David Sauer <davedamoon@gmail.com>
2021-02-04 19:08:07 +0100

    fixed docker inspect test

--- tests/docker-inspect.sh
+++ tests/docker-inspect.sh
@@ -3,7 +3,7 @@ set -uexo pipefail
 export DOCKER_HOST=tcp://127.0.0.1:12376
 until docker version; do sleep 1; done
 
+docker pull alpine
 docker inspect alpine
-if docker pull missing; then echo "this should fail"; false; else echo "exit code propagated"; fi
 
 echo "done"
